76ec7277c831150b006a029d7306b5a9207e2735
Bohdan Raievskyi <raevskijb@gmail.com>
2025-12-20 22:46:18 +0100

    Mark `HashSaltRepository` as repository

--- backend/src/main/java/com/example/backend/repository/HashSaltRepository.java
+++ backend/src/main/java/com/example/backend/repository/HashSaltRepository.java
@@ -2,8 +2,11 @@ package com.example.backend.repository;
 
 import com.example.backend.model.security.UserHashSalt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface HashSaltRepository extends JpaRepository<UserHashSalt, Long> {
     Optional<UserHashSalt> findByUserId(Long userId);
 }
