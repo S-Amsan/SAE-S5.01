153d3b80e588b359b21b42612a1b20634483155f
Bohdan Raievskyi <raevskijb@gmail.com>
2025-12-23 13:48:37 +0100

    Add Post entity and repository

---
+++ backend/src/main/java/com/example/backend/model/Post.java
@@ -0,0 +1,36 @@
+package com.example.backend.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
