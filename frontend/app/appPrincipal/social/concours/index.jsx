3072125827b4a88f87326db5b09344d98f1e712d
Bohdan Raievskyi <raevskijb@gmail.com>
2026-01-01 23:53:53 +0100

    Fetch following competitions

--- frontend/app/appPrincipal/social/concours/index.jsx
+++ frontend/app/appPrincipal/social/concours/index.jsx
@@ -2,48 +2,17 @@ import React from "react";
 
 import EventPage from "../_components/EventPage";
 
-import { fetchLatestCompetition } from "../../../../services/competitions.api"
+import { fetchLatestCompetition, fetchFollowingCompetitions } from "../../../../services/competitions.api"
 
 export default function Concours() {
-    const concours_DATA = fetchLatestCompetition(); // le concours le plus récent et pas fini, Date_fin > date d'aujourd'hui
+    const [concours_DATA, setConcoursData] = React.useState(null);
+    const [user_event_DATA, setUserEventData] = React.useState(null);
 
-    const user_event_DATA = [{
-        Nom : "Novembre 2025",
-        Date_fin : "2025-11-30T23:59:59",
-        Points_objectif : 10000,
-        Points_recolte : 12000,
-        Recompense : null,
-        Participants : 112,
-        Qualifies : 54,
-        Cout_inscription : 1000,
-    },{
-        Nom : "Septembre 2025",
-        Date_fin : "2025-09-30T23:59:59",
-        Points_objectif : 10000,
-        Points_recolte : 500,
-        Recompense : null,
-        Participants : 112,
-        Qualifies : 54,
-        Cout_inscription : 1000,
-    },{
-        Nom : "Juin 2025",
-        Date_fin : "2025-06-30T23:59:59",
-        Points_objectif : 10000,
-        Points_recolte : 42332,
-        Recompense : null,
-        Participants : 112,
-        Qualifies : 54,
-        Cout_inscription : 1000,
-    },{
-        Nom : "Mai 2025",
-        Date_fin : "2025-05-30T23:59:59",
-        Points_objectif : 10000,
-        Points_recolte : 15000,
-        Recompense : {Nom : "E-carte cadeau 25€ (Amazon)"},
-        Participants : 112,
-        Qualifies : 54,
-        Cout_inscription : 1000,
-    }]; //TODO récupérer les vrai données (Tous les concours où l'utilisateur connecté s'est inscrit) -> renvoyer null si pas de concours
+    React.useEffect(() => {
+        fetchLatestCompetition().then(setConcoursData); // le concours le plus récent et pas fini, Date_fin > date d'aujourd'hui
+
+        fetchFollowingCompetitions().then(setUserEventData); // Tous les concours où l'utilisateur connecté s'est inscrit -> renvoyer null si pas de concours
+    }, []);
 
     return <EventPage type={"concours"} event_DATA={concours_DATA} user_event_DATA={user_event_DATA}/>
 };
--- frontend/services/competitions.api.js
+++ frontend/services/competitions.api.js
@@ -1,4 +1,5 @@
 import { Platform } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const API_URL =
     Platform.OS === "android"
@@ -45,4 +46,31 @@ export async function fetchLatestCompetition() {
     const res = await fetch(`${API_URL}/competition/latest`);
     const competition = await res.json();
     return competition;
+}
+
+/**
+ * Response example:
+ * ```js
+ * [
+ *   {
+ *     "id": 1,
+ *     "name": "Décembre 2025",
+ *     "deadline": "2025-12-30T17:59:59.000+00:00",
+ *     "goalPoints": 10000,
+ *     "participants": 112,
+ *     "qualified": 54,
+ *     "inscriptionCost": 1000
+ *   }
+ * ]
+ * ```
+*/
+export async function fetchFollowingCompetitions() {
+    const token = await AsyncStorage.getItem('@auth_token');
+    const res = await fetch(`${API_URL}/competition/following`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+    const competitions = await res.json();
+    return competitions;
 }
